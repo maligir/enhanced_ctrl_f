03635dcf901a47433550a280c3abdfeb4d22bc79
Rahul Maligi <maligir@utexas.edu>
2023-05-26 18:57:48 -0500

    created production model

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re, string
-from transformers import pipeline
-
-import nltk
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 app = Flask(__name__)
 CORS(app)
 
-nlp = pipeline('question-answering')  # Initialize the question-answering pipeline
-
-def preprocess_text(text):
-    text = text.lower()  # Convert text to lower case
-    text = text.translate(str.maketrans('', '', string.punctuation))  # Remove punctuation
-    stop_words = set(stopwords.words('english'))  # Remove stopwords
-    words = text.split()
-    words = [word for word in words if word not in stop_words]
-    return ' '.join(words)
+model_name = "deepset/roberta-base-squad2"
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -42,21 +22,16 @@ def search():
     if not query or query.strip() == '':
         return jsonify(results=[{'text': 'The search query is empty.', 'expandedText': 'The search query is empty.'}])
 
-    text = preprocess_text(text)
+    # text = preprocess_text(text)
     
     results = []
-    answers = nlp(question=query, context=text, topk=5)  # Get top 5 answers
+    answers = nlp({'question': query, 'context': text})  # Get top 5 answers
+    results.append({'text': answers['answer'] if len(answers['answer']) < 100 else answers['answer'][:100], 'expandedText': answers['answer']})
     
     if not answers:
         return jsonify(results=[{'text': 'No answers found.', 'expandedText': 'No answers found.'}])
 
-    for ans in answers:
-        start = max(0, ans['start'] - 20)
-        end = min(len(text), ans['end'] + 20)
-        results.append({
-            'text': ans['answer'],
-            'expandedText': text[start:end],
-        })
+    print(answers)
 
     return jsonify(results=results)
 
